13a9f9dabad36225c08ddf0fed55db571c5003c3
vidhurvoora <lab.nirvana@gmail.com>
2017-03-15 22:48:23 -0700

    Initial integration of Data binding

--- sample/Flickster/app/src/main/java/com/codepath/flickster/activities/MovieDetailsActivity.java
+++ sample/Flickster/app/src/main/java/com/codepath/flickster/activities/MovieDetailsActivity.java
@@ -1,35 +1,32 @@
 package com.codepath.flickster.activities;
 
 import android.app.Activity;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.RatingBar;
 import android.widget.TextView;
 
 import com.codepath.flickster.R;
+import com.codepath.flickster.databinding.ActivityDetailsBinding;
 import com.codepath.flickster.models.Movie;
 import com.codepath.flickster.utils.MovieImagePathUtils;
 import com.squareup.picasso.Picasso;
 
-import butterknife.BindView;
-import butterknife.ButterKnife;
-
 public class MovieDetailsActivity extends Activity {
     private static final String TAG = MovieDetailsActivity.class.getSimpleName();
-
-    @BindView(R.id.ivBackdrop) ImageView ivBackdrop;
-    @BindView(R.id.tvTitle) TextView tvTitle;
+    ActivityDetailsBinding detailsBinding;
+    ImageView ivBackdrop;
+    TextView tvTitle;
 
     private Movie movie;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_details);
-        ButterKnife.bind(this);
-
+        detailsBinding = DataBindingUtil.setContentView(this,R.layout.activity_details);
+        ivBackdrop = detailsBinding.ivBackdrop;
+        tvTitle = detailsBinding.tvTitle;
         movie = getIntent().getParcelableExtra("movie");
         if (movie != null) {
             initView();
