c37cb52deb1f405650c362916bc20316f5a9529f
vidhurvoora <lab.nirvana@gmail.com>
2017-03-15 23:39:31 -0700

    Implemented one-way data binding

--- sample/Flickster/app/src/main/java/com/codepath/flickster/activities/MovieDetailsActivity.java
+++ sample/Flickster/app/src/main/java/com/codepath/flickster/activities/MovieDetailsActivity.java
@@ -3,21 +3,15 @@ package com.codepath.flickster.activities;
 import android.app.Activity;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.ImageView;
-import android.widget.TextView;
 
 import com.codepath.flickster.R;
 import com.codepath.flickster.databinding.ActivityDetailsBinding;
 import com.codepath.flickster.models.Movie;
 import com.codepath.flickster.utils.MovieImagePathUtils;
-import com.squareup.picasso.Picasso;
 
 public class MovieDetailsActivity extends Activity {
     private static final String TAG = MovieDetailsActivity.class.getSimpleName();
     ActivityDetailsBinding detailsBinding;
-    ImageView ivBackdrop;
-    TextView tvTitle;
 
     private Movie movie;
 
@@ -25,18 +19,7 @@ public class MovieDetailsActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         detailsBinding = DataBindingUtil.setContentView(this,R.layout.activity_details);
-        ivBackdrop = detailsBinding.ivBackdrop;
-        tvTitle = detailsBinding.tvTitle;
         movie = getIntent().getParcelableExtra("movie");
-        if (movie != null) {
-            initView();
-        } else {
-            Log.e(TAG, "Movie is NULL");
-        }
-    }
-
-    private void initView() {
-        Picasso.with(this).load(MovieImagePathUtils.getBackdropImagePath(movie)).into(ivBackdrop);
-        tvTitle.setText(movie.getOriginalTitle());
+        detailsBinding.setMovie(movie);
     }
 }
--- sample/Flickster/app/src/main/java/com/codepath/flickster/adapters/MoviesAdapter.java
+++ sample/Flickster/app/src/main/java/com/codepath/flickster/adapters/MoviesAdapter.java
@@ -6,14 +6,11 @@ import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.TextView;
 
 import com.codepath.flickster.R;
 import com.codepath.flickster.databinding.ItemMovieBinding;
 import com.codepath.flickster.models.Movie;
 import com.codepath.flickster.utils.MovieImagePathUtils;
-import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
@@ -39,9 +36,7 @@ public class MoviesAdapter extends RecyclerView.Adapter<MoviesAdapter.ViewHolder
     public void onBindViewHolder(MoviesAdapter.ViewHolder holder, int position) {
         Movie movie = movieList.get(position);
         if (movie != null) {
-            Picasso.with(context).load(MovieImagePathUtils.getPosterImagePath(movie)).into(holder.ivPoster);
-            holder.tvTitle.setText(movie.getOriginalTitle());
-            holder.tvDescription.setText(movie.getOverview());
+            holder.itemMovieBinding.setMovie(movie);
         } else {
             Log.e(TAG, "Movie is NULL");
         }
@@ -53,15 +48,11 @@ public class MoviesAdapter extends RecyclerView.Adapter<MoviesAdapter.ViewHolder
     }
 
     class ViewHolder extends RecyclerView.ViewHolder {
-        ImageView ivPoster;
-        TextView tvTitle;
-        TextView tvDescription;
+        ItemMovieBinding itemMovieBinding;
 
         ViewHolder(ItemMovieBinding movieBinding) {
             super(movieBinding.getRoot());
-            ivPoster = movieBinding.ivPoster;
-            tvTitle = movieBinding.tvTitle;
-            tvDescription = movieBinding.tvDescription;
+            itemMovieBinding = movieBinding;
         }
     }
 }
