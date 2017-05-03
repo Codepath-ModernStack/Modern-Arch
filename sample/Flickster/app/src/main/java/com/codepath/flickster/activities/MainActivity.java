f806b623158753294dab8f1006e505a17be54f76
janechung <jane@coinbase.com>
2017-05-02 19:46:16 -0700

    MVP

--- sample/Flickster/app/src/main/java/com/codepath/flickster/activities/MainActivity.java
+++ sample/Flickster/app/src/main/java/com/codepath/flickster/activities/MainActivity.java
@@ -6,16 +6,14 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.View;
 
-import com.codepath.flickster.MovieApp;
-import com.codepath.flickster.models.Movie;
-import com.codepath.flickster.models.MovieResponse;
-import com.codepath.flickster.networking.MovieRestClient;
+import com.codepath.flickster.ActivityScope;
+import com.codepath.flickster.ComponentProvider;
 import com.codepath.flickster.R;
 import com.codepath.flickster.adapters.MoviesAdapter;
 import com.codepath.flickster.databinding.ActivityMainBinding;
+import com.codepath.flickster.models.Movie;
 import com.codepath.flickster.views.DividerItemDecoration;
 import com.codepath.flickster.views.ItemClickSupport;
 
@@ -24,23 +22,25 @@ import java.util.List;
 
 import javax.inject.Inject;
 
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-
-public class MainActivity extends AppCompatActivity {
-    private static final String TAG = MainActivity.class.getSimpleName();
+@ActivityScope
+public class MainActivity extends AppCompatActivity implements MainScreen {
     private ActivityMainBinding mainBinding;
 
-    @Inject MovieRestClient movieRestClient;
     private List<Movie> movieList;
     private MoviesAdapter adapter;
     private RecyclerView rvMovies;
 
+    @Inject
+    MainPresenter mPresenter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        MovieApp.getApp().getAppComponent().inject(this);
+
+        ((ComponentProvider) getApplicationContext())
+                .applicationComponent()
+                .mainActivitySubcomponent(new MainPresenterModule(this))
+                .inject(this);
 
         mainBinding = DataBindingUtil.setContentView(this,R.layout.activity_main);
         rvMovies = mainBinding.rvMovies;
@@ -63,13 +63,11 @@ public class MainActivity extends AppCompatActivity {
                         Movie movie = movieList.get(position);
                         if (movie != null) {
                             gotoMovieDetails(movie);
-                        } else {
-                            Log.e(TAG, "Movie is NULL");
                         }
                     }
                 });
 
-        populateMovieList();
+        mPresenter.populateMovieList();
     }
 
     private void gotoMovieDetails(Movie movie) {
@@ -78,29 +76,9 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    protected void updateMovieList(List<Movie> movies) {
+    @Override
+    public void updateMovieList(List<Movie> movies) {
         movieList.addAll(movies);
         adapter.notifyDataSetChanged();
-    }
-
-    protected void populateMovieList() {
-        // https://developers.themoviedb.org/3/movies/get-now-playing
-        movieRestClient.nowPlaying(new Callback<MovieResponse>(){
-            @Override
-            public void onResponse(Call<MovieResponse> call, Response<MovieResponse> response) {
-                Log.i(TAG, "onResponse");
-                if(response != null) {
-                    MovieResponse movieResponse = response.body();
-                    if(movieResponse != null && movieResponse.getResults() != null) {
-                        updateMovieList(movieResponse.getResults());
-                    }
-                }
-            }
-
-            @Override
-            public void onFailure(Call<MovieResponse> call, Throwable t) {
-                Log.e(TAG, "onFailure");
-            }
-        });
     }
 }
--- sample/Flickster/app/src/main/java/com/codepath/flickster/networking/MovieRestClient.java
+++ sample/Flickster/app/src/main/java/com/codepath/flickster/networking/MovieRestClient.java
@@ -1,26 +1,35 @@
 package com.codepath.flickster.networking;
 
-import com.codepath.flickster.MovieApp;
 import com.codepath.flickster.models.MovieResponse;
 
-import javax.inject.Inject;
-
+import okhttp3.OkHttpClient;
 import retrofit2.Call;
 import retrofit2.Callback;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MovieRestClient {
 
     private final String API_KEY = "a07e22bc18f5cb106bfe4cc1f83ad8ed";
 
-    @Inject public MovieApiInterface apiInterface;
+    private synchronized MovieApiInterface getService() {
+        OkHttpClient okHttpClient = new OkHttpClient.Builder().build();
 
-    public MovieRestClient() {
-        MovieApp.getApp().getAppComponent().inject(this);
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://api.themoviedb.org/3/")
+                .client(okHttpClient)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        MovieApiInterface service = retrofit.create(MovieApiInterface.class);
+
+        return service;
     }
 
     // http://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed
     public void nowPlaying(Callback<MovieResponse> responseHandler) {
-        Call<MovieResponse> call = apiInterface.getNowPlayingMovies(API_KEY);
+        MovieApiInterface movieApiInterface = getService();
+        Call<MovieResponse> call = movieApiInterface.getNowPlayingMovies(API_KEY);
         call.enqueue(responseHandler);
     }
 }
