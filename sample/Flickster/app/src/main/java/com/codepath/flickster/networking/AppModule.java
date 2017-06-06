61306907cf163c27eef23ecb688081aaec7dd5ac
Yoav Gray <yoav@autolist.com>
2017-06-05 21:03:34 -0700

    Change field to private

--- sample/Flickster/app/src/main/java/com/codepath/flickster/networking/AppModule.java
+++ sample/Flickster/app/src/main/java/com/codepath/flickster/networking/AppModule.java
@@ -10,7 +10,7 @@ import dagger.Provides;
 @Module
 public class AppModule {
 
-    Application mApplication;
+    private Application mApplication;
 
     public AppModule(Application application) {
         mApplication = application;
